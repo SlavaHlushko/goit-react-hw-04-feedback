90decd70fb1a26ec2a190ed574af18f8b37f707d
SlavaHlushko <slavahlushko90@gmail.com>
2022-12-04 18:40:26 +0200

    rename folder

--- src/components/feedbackList/FeedbackList.jsx
+++ src/components/feedbackList/FeedbackList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import {
   ButtonList,
   ButtonItem,
-} from 'components/feedbackList/FeedbackList.styled';
+} from 'components/FeedbackList/FeedbackList.styled';
 
 export const FeedbackList = ({ options, onLeaveFeedback }) => {
   return (
